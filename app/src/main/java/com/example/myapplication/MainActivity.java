c138078748a1235728b8ae6ae3986f5130282a2c
git commit -F /mnt/c/Users/VADIMS~1/AppData/Local/Temp/git-commit-msg-.txt --author="2VadimSH <vadimshaposhnik004@gmail.com>
2024-02-08 17:00:20 +0200

    added animation (calendar, +)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
-import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
@@ -14,6 +17,7 @@ import com.example.myapplication.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private Button opCal;
+    private Button openBsBtn;
 
     private ActivityMainBinding binding;
 
@@ -22,33 +26,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        opCal = findViewById(R.id.openCalendar);
 
+        opCal = binding.openCalendar;
+        openBsBtn = binding.openBsBtn;
+
+        applyClickAnimation(opCal);
+        applyClickAnimation(openBsBtn);
 
         binding.openBsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Animation scaleAnim = new ScaleAnimation(1f, 0.8f, 1f, 0.8f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                scaleAnim.setDuration(100);
+
+                Animation alphaAnim = new AlphaAnimation(1f, 0.7f);
+                alphaAnim.setDuration(100);
+
+                AnimationSet animationSet = new AnimationSet(true);
+                animationSet.addAnimation(scaleAnim);
+                animationSet.addAnimation(alphaAnim);
+
+                v.startAnimation(animationSet);
                 MyBottomSheet myBottomsheet = new MyBottomSheet();
-                myBottomsheet.show(getSupportFragmentManager(),"my bottom sheet dialog");
+                myBottomsheet.show(getSupportFragmentManager(), "my bottom sheet dialog");
             }
         });
+    }
 
-        opCal.setOnClickListener(new View.OnClickListener() {
+    private void applyClickAnimation(final Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Animation scaleAnim = new ScaleAnimation(1f, 0.8f, 1f, 0.8f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                scaleAnim.setDuration(100);
+
+                Animation alphaAnim = new AlphaAnimation(1f, 0.7f);
+                alphaAnim.setDuration(100);
+
+                AnimationSet animationSet = new AnimationSet(true);
+                animationSet.addAnimation(scaleAnim);
+                animationSet.addAnimation(alphaAnim);
+
+                v.startAnimation(animationSet);
+
                 Intent intent = new Intent(MainActivity.this, ActivityCalendar.class);
                 startActivity(intent);
             }
         });
-
-
-
-
-
     }
 }
